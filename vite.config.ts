60a96a8ec2b4535f1eb489142ce4cf2559d6787f
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-04-24 19:11:04 +0200

    Adjust vite config for docker ci allowance

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const containerName = env.CONTAINER_NAME || "localhost";
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      host: true,
+      port: 4173,
+    },
+    preview: {
+      port: 4173,
+      host: true,
+      allowedHosts: [".localhost", containerName],
+    },
+  };
 });
